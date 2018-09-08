4487d6e6f1bda0a278f172d64d9aadd64e7c7ff4
Ellen <ellen.bacerra@gmail.com>
2018-09-07 23:20:29 -0500

    added necs folders and files

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+var tables = [];
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
